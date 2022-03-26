92c5f82333152e527e02e03f75bdd524cbd90b7d
Vitor Benedito <vitor.bsdc@gmail.com>
2022-03-26 19:03:23 -0300

    .

--- src/componentes/Cards/ToggleButton/index.js
+++ src/componentes/Cards/ToggleButton/index.js
@@ -4,13 +4,11 @@ import "./style.css";
 const ToggleButton = () => {
   return (
     <div className="container">
-      <div className="card">
         <h3 className="content">Desligado / Ligado</h3>
         <label className="switch">
           <input type="checkbox" />
           <span className="slider" />
         </label>
-      </div>
     </div>
   );
 };
